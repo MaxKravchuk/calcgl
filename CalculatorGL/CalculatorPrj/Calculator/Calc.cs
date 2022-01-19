6d60d06ee20d817395c31bea433ef6b1807c66a2
MaxKravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-01-19 17:40:22 +0200

    simple console calc

---
+++ CalculatorGL/CalculatorPrj/Calculator/Calc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        private readonly List<int> addents;
+        private readonly List<string> operations;
+
+        public Calc()
+        {
+            addents = new List<int>();
+            operations = new List<string>();
+        }
+
+        public int Result(string expression)
+        {
+            PrepareString(expression);
+            Solve("*", (a, b) => a * b, "/", (a, b) => a / b);
+            Solve("+", (a, b) => a + b, "-", (a, b) => a - b);
+
+            return addents[0];
+        }
+
+        private void PrepareString(string expression)
+        {
+            addents.Clear();
+            operations.Clear();
+
+            if (expression.Length == 0) throw new ArgumentException("Pls enter expression");
+            var parts = expression.Split(' ');
+
+            for (var i = 0; i < parts.Length; i += 2)
+            {
+                addents.Add(Convert.ToInt32(parts[i]));
+                if (i + 1 < parts.Length)
+                {
+                    if (!(parts[i + 1].Contains("+") || parts[i + 1].Contains("-")|| parts[i + 1].Contains("/") || parts[i + 1].Contains("*"))) throw new ArithmeticException("Wrong operation! Use \"+\" \"-\" \"*\" \"/\"");
+                    operations.Add(parts[i + 1]);
+                }
+            }
+        }
+
+        private void Solve(string currentOperation1, Func<int, int, int> function1, string currentOperation2, Func<int, int, int> function2)
+        {
+            while (true)
+            {
+
+                var i1 = operations.IndexOf(currentOperation1);
+                var i2 = operations.IndexOf(currentOperation2);
+
+                int index;
+                if (i1 >= 0 && i2 >= 0)
+                {
+                    index = Math.Min(i1, i2);
+                }
+                else
+                {
+                    index = Math.Max(i1, i2);
+                }
+
+                if (index < 0)
+                {
+                    break;
+                }
+
+                if (index == i1)
+                {
+                    addents[index] = function1(addents[index], addents[index + 1]);
+                }
+                else
+                {
+                    addents[index] = function2(addents[index], addents[index + 1]);
+                }
+
+                operations.RemoveAt(index);
+                addents.RemoveAt(index + 1);
+            }
+        }
+    }
+}
---
+++ CalculatorGL/CalculatorPrj/CalculatorPrj/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Calculator;
+
+namespace CalculatorPrj
+{
+    class Program
+    {
+        static void Main()
+        {
+            TurnOnCalculator();
+        }
+
+        static void TurnOnCalculator()
+        {
+            Calc c = new();
+            while (true)
+            {
+                Console.WriteLine("Enter the expression separating each element with a space\nStop -> enter 0");
+                Console.Write("Expression -> ");
+                var expression = Console.ReadLine();
+                if (expression.Contains('0')) break;
+                try
+                {
+                    Console.WriteLine($"Result {expression} = {c.Result(expression)}");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
